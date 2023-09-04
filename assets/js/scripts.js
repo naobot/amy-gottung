17dde1e16ac3dd7555a599b61b56ff6ae7261a4c
Naomi Cui <naomi@nowme.ca>
2023-09-04 16:18:28 -0400

    fix showdown converter; change blue

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,8 +2,6 @@ window.addEventListener('load', function () {
   var converter = new showdown.Converter();
   for (const elem of document.getElementsByClassName('convert-md')) {
     var md = elem.dataset.md;
-    md = md.replace(/\n{2,}/g, m => m.replace(/\n/g, "<br/>"));
-    md = md.replace(/<br\/>([^<])/g, "<br\/>\n\n$1");
     var html = converter.makeHtml(md);
     elem.innerHTML = html;
   }
