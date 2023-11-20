b30b2e0815a4da4079b301b0530ee25d5021a090
naomi cui <naomi@nowme.ca>
2023-11-20 15:25:47 -0500

    fix external link bug for convert-md content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,20 @@
+let internal = location.host.replace('www', '');
+internal = new RegExp('internal', 'i');
+
 window.addEventListener('load', function () {
   var converter = new showdown.Converter();
   for (const elem of document.getElementsByClassName('convert-md')) {
     var md = elem.dataset.md;
     var html = converter.makeHtml(md);
     elem.innerHTML = html;
+
+    // external links open in new tab
+    // (jekyll-target-blank does not work on yml data)
+    for (const link of elem.getElementsByTagName('a')) {
+      const href = link.host;
+      if (!internal.test(href)) {
+        link.setAttribute('target', '_blank');
+      }
+    }
   }
 });
